dff37655c9094562aa9cc6842c8755c3f2add5be
Phoebe Wong <phoebe.wong@berkeley.edu>
2018-10-18 17:33:30 -0400

    Update indents and add gitignore

--- roots.py
+++ roots.py
@@ -51,8 +51,8 @@ def quad_roots(a=1.0, b=2.0, c=0.0):
     if a == 0:
         raise ValueError("The quadratic coefficient is zero.  This is not a quadratic equation.")
     else:
-    sqrtdisc = cmath.sqrt(b * b - 4.0 * a * c)
-    r1 = -b + sqrtdisc
+        sqrtdisc = cmath.sqrt(b * b - 4.0 * a * c)
+        r1 = -b + sqrtdisc
         r2 = -b - sqrtdisc
         twoa = 2.0 * a
         return (r1 / twoa, r2 / twoa)
